c1c81942c64797716542acf9afa069e67c7d29b4
Yaniv Efraim <yaniv.efraim@gmail.com>
2018-10-04 23:15:03 +0300

    create-yoshi-app: sort projects by priority

--- packages/create-yoshi-app/src/getProjectTypes.js
+++ packages/create-yoshi-app/src/getProjectTypes.js
@@ -1,9 +1,14 @@
 const path = require('path');
 const globby = require('globby');
+const sortedProjects = require('./sortedProjects');
 
 module.exports = () => {
-  return globby.sync('*', {
-    cwd: path.join(__dirname, '../templates'),
-    onlyFiles: false,
-  });
+  return globby
+    .sync('*', {
+      cwd: path.join(__dirname, '../templates'),
+      onlyFiles: false,
+    })
+    .sort((a, b) => {
+      return sortedProjects.indexOf(a) - sortedProjects.indexOf(b);
+    });
 };
